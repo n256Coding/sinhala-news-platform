951650ec0de2da38531c5683ef442fdc768ce8a6
Nishan <nishan256@gmail.com>
2024-06-22 12:03:05 +0100

    initialize the db model and basic views

--- news_app/models.py
+++ news_app/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+class News(models.Model):
+    date = models.DateField()
+    heading = models.TextField()
+    category = models.CharField(max_length=50)
+    link_to_source = models.TextField()

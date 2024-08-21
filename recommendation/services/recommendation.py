f0813e26b455df49759c21cad8d9975102e0e26b
Nishan <nishan256@gmail.com>
2024-08-21 11:14:52 +0100

    use metrix factorization to handle sparcity and cold start recommendations

--- recommendation/services/recommendation.py
+++ recommendation/services/recommendation.py
@@ -10,6 +10,8 @@ from recommendation.models import UserFeedback
 from sinling import SinhalaTokenizer
 import numpy as np
 from gensim.models import FastText
+from sklearn.decomposition import NMF
+from django.contrib.auth.models import User
 
 from recommendation.services.vector_db_provider import get_chroma_db_collection
 from sinhala_news_platform_backend.settings import MAXIMUM_NO_OF_SUGGESTED_ARTICLES
@@ -44,7 +46,7 @@ def to_sentence_embedding(sentence: str):
 
     return wv_model.wv.get_sentence_vector(sentence)
 
-def get_recommended_articles(todays_news_items, logger: Logger, current_user):
+def get_recommended_articles(todays_news_items, logger: Logger, current_user: User):
 
     chroma_collection = get_chroma_db_collection()
 
@@ -89,12 +91,68 @@ def get_recommended_articles(todays_news_items, logger: Logger, current_user):
     
     else:
         # The user seems to be new user or they have never liked/disliked any article before
-        # Suggest some random articles
-        logger.info('User has not liked any previous articles. Suggesting some random articles.')
+        # Suggest some articles based on matrix factorization
+        logger.info('User has not liked any previous articles. Suggesting articles using matrix factorization.')
 
-        todays_news_items = [news_item for news_item in todays_news_items]
+        all_users = User.objects.all()
+        user_id_list = [user.id for user in all_users]
+        news_id_list = [news_item.id for news_item in todays_news_items]
 
-        resulted_articles = random.sample(todays_news_items, MAXIMUM_NO_OF_SUGGESTED_ARTICLES)
+        current_user_id = current_user.id
+        news_item_map: dict = dict()
+
+        matrix = []
+        for i, user_id in enumerate(user_id_list):
+
+            if user_id == current_user.id:
+                # Find the row index of matrix of current user
+                current_user_id = i
+
+            user_vector = []
+            for i, news_id in enumerate(news_id_list):
+                news_item_map[i] = news_id
+                user_vector.append(int(UserFeedback.objects.filter(news_item_id=news_id, user_id=user_id).exists()))
+
+            matrix.append(user_vector)
+
+        # This sparse matrix would look like below where the rows represents a user and columns represents the news items
+        # R = np.array([
+        #     [0, 0, 0, 0],
+        #     [1, 0, 0, 1],
+        #     [1, 1, 0, 1],
+        #     [1, 0, 0, 1],
+        #     [0, 1, 1, 1],
+        # ])
+        R = np.array(matrix)
+
+        # Perform matrix factorization
+        model = NMF(n_components=2, init='random', random_state=0)
+        U = model.fit_transform(R)
+        V = model.components_
+
+        def recommend_items(user_id, n):
+            if np.all(R[user_id] == 0):
+                # For new users, recommend based on overall popularity
+                item_scores = np.sum(R, axis=0)
+            else:
+                # Predict interactions for all items
+                item_scores = np.dot(U[user_id], V)
+            
+            # Get indices of top N items, excluding already interacted items
+            interacted_items = np.where(R[user_id] > 0)[0]
+            uninteracted_items = np.setdiff1d(np.arange(R.shape[1]), interacted_items)
+            top_items = uninteracted_items[np.argsort(item_scores[uninteracted_items])[-n:][::-1]]
+            
+            return top_items
+
+        
+        recommended_items = recommend_items(current_user_id, n=MAXIMUM_NO_OF_SUGGESTED_ARTICLES)
+
+        # Translate matrix index to actual news id
+        recommended_items = [news_item_map.get(item) for item in recommended_items]
+        print(f"Recommended items for user {current_user_id}: {recommended_items}")
+
+        resulted_articles = News.objects.filter(pk__in=recommended_items).all()
 
         logger.info(resulted_articles)
 
